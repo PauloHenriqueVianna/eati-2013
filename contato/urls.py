e430a09e2ada0994f20c783f0512dfb61edb2144
PauloHenriqueVianna <paulo@note-pronatec.(none)>
2013-07-25 13:13:58 -0300

    add formulário fale conosco

--- contato/urls.py
+++ contato/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from views import apoio, enviar
+from views import apoio, fale, enviar
 
 urlpatterns = patterns('contato',
     #url(r'^$', Inicio),
     url(r'^apoio/$',apoio),
+    url(r'^fale-conosco/$',fale),
     url(r'^enviar/$',enviar),
 )
--- contato/views.py
+++ contato/views.py
@@ -7,23 +7,36 @@ def apoio(request):
 	ctoken.update(csrf(request))
 	return render_to_response('apoio.html', RequestContext(request, {}))
 
+def fale(request):
+	ctoken = {}
+	ctoken.update(csrf(request))
+	return render_to_response('fale-conosco.html', RequestContext(request, {}))
+
 def enviar(request):
 	ctoken = {}
 	ctoken.update(csrf(request))
 	if request.method == 'POST':
 		nome = request.POST.get('name')
-		empresa = request.POST.get('company')
 		email = request.POST.get('email')
 		message = request.POST.get('message')
-		if empresa != '':
-			mensagem = '<b>Nome:</b> ' + nome + ' <br> <b>Empresa:</b> ' + empresa + ' <br><br> ' + message
+		flag = request.POST.get('flag')
+		if flag == 'apoio':
+			empresa = request.POST.get('company')
+			empresa_assunto = empresa
+			assunto = 'Apoio IV EATI'
+			pagina = 'apoio.html'
+			if empresa != '':
+				mensagem = '<b>Nome:</b> ' + nome + ' <br> <b>Empresa:</b> ' + empresa + ' <br><br> ' + message
+			else:
+				mensagem = '<b>Nome:</b> ' + nome + ' <br><br> ' + message
 		else:
+			assunto = request.POST.get('subject')
+			empresa_assunto = assunto
+			pagina = 'fale-conosco.html'
 			mensagem = '<b>Nome:</b> ' + nome + ' <br><br> ' + message
-		msg = EmailMessage('Apoio IV EATI', mensagem, email, ['eati.organizacao@cafw.ufsm.br'],headers = {'Reply-To': email})
+		msg = EmailMessage(assunto, mensagem, email, ['eati.organizacao@cafw.ufsm.br'],headers = {'Reply-To': email})
 		msg.content_subtype = "html"
 		if msg.send():
-			return render_to_response('apoio.html', RequestContext(request, {'enviado':1}))
+			return render_to_response(pagina, RequestContext(request, {'enviado':1}))
 		else:
-			return render_to_response('apoio.html', RequestContext(request, {'enviado':2,'nome':nome,'empresa':empresa,'email':email,'mensagem':message}))
-
-
+			return render_to_response(pagina, RequestContext(request, {'enviado':2,'nome':nome,'empresa_assunto':empresa_assunto,'email':email,'mensagem':message}))
