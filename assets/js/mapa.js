a41c58ef8cbe23534067e61826a38efff70a0475
PauloHenriqueVianna <paulo@note-pronatec.(none)>
2013-08-01 12:40:22 -0300

    novo logo softex e alteração altura mapa

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -1,22 +1,25 @@
 
-var latlng = new google.maps.LatLng(-27.389754,-53.431654);
+var latlng = new google.maps.LatLng(-27.347984,-53.425537);
+var point = new google.maps.LatLng(-27.389754,-53.431654);
 
 var myOptions = {
-  zoom: 8,
+  zoom: 11,
   center: latlng,
   mapTypeControl: true,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map = new google.maps.Map(document.getElementById("mapa"), myOptions);
-var contentString = '<b>Colégio Agrícola de Frederico Westphalen</b><br>Encontro Anual de Tecnologia da Informação';
+var contentString = '<div class="row-fluid"><div class="span3"><img src="../assets/img/logo-eati-maps.png"></div><div class="span9"><h2><b>Encontro Anual de Tecnologia da Informação</b></h2></div></div>';
 var infowindow = new google.maps.InfoWindow({
     content: contentString
 });
 var marker = new google.maps.Marker({
-	    position: latlng,
+	    position: point,
 	    map: map,
 	    title: 'Colégio Agrícola de Frederico Westphalen'
 	  	});
+marker.setIcon('../assets/img/computers.png');
+infowindow.open(map,marker);
 google.maps.event.addListener(marker, 'click', function() {
 	infowindow.open(map,marker);
 });
